298be710a8154597db2393dc2d9bf76e38ccafd4
Amit Gupta <your@email.com>
2025-12-27 01:31:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    if(req.url ==  "/about"){
+        res.end ("The about page    jiiii  kii here")
+    }
+
+    if(req.url == "/profile"){
+        res.end ("I Am Amit Gupta")
+    }
+
+    if(req.url == "/") {
+        res.end("im home page")
+    }
+   
+    
+
+})
+
+
+server.listen(3000, () => console.log('Server running on port 3000'))
