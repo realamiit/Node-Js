f3b7adb829c5835404b5bdef4ecb13ac9e870a8e
Amit Gupta <your@email.com>
2025-12-28 01:26:04 +0530

    refactor: improve app structure and update index.ejs form

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 var morgan = require('morgan')
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.use(morgan('dev'))
 
-})
+app.set("view engine", "ejs");
 
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.get('/about', (req, res) => {
     res.send('I am about page also')
@@ -27,4 +30,11 @@ app.get('/papaya', (req, res) => {
     res.send('papaya is a fruits')
 
 })
+
+app.get('/get-from-data', (req,res) =>{
+    console.log(req.querry)
+    res.send('data recived')
+})
+
+
 app.listen(3000)
