7390305969b873fcbe7bc7bad0116d2ea29d9756
Amit Gupta <amitgupta4263@gmail.com>
2026-05-10 00:44:01 +0530

    feat: add additional math constants and example outputs to script

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ const math = require("./math");
 console.log(math.sum(2,2));
 console.log(math.mul(2,4));
 console.log(math.div(4,2));
+console.log(math.PI);
+console.log(math.g);
+
+
+// reala@LAPTOP-LLMHO5F9 MINGW64 ~/OneDrive/Desktop/Node Js
+// $ node script.js
+// 4
+// 8
+// 2
+// 3.14
+// 9.09
