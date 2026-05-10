594e22f55c9bc05c49b84d81c5d153b02b0d079b
Amit Gupta <amitgupta4263@gmail.com>
2026-05-10 23:45:16 +0530

    refactor: comment out math function calls in script.js for clarity

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Basicly its calls (used) function by the math.js 
-const math = require("./math");
+// const math = require("./math");
 
 
-console.log(math.sum(2,2));
-console.log(math.mul(2,4));
-console.log(math.div(4,2));
-console.log(math.PI);
-console.log(math.g);
+// console.log(math.sum(2,2));
+// console.log(math.mul(2,4));
+// console.log(math.div(4,2));
+// console.log(math.PI);
+// console.log(math.g);
 
 
 // reala@LAPTOP-LLMHO5F9 MINGW64 ~/OneDrive/Desktop/Node Js
@@ -16,3 +16,11 @@ console.log(math.g);
 // 2
 // 3.14
 // 9.09
+
+
+
+const info = require("./fruits");
+
+
+console.log(info);
+// console.log(info[0]);
