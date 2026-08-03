c4e071293b7d10bfead1c937e1b2659060817329
Amit Gupta <amitgupta4263@gmail.com>
2026-08-03 21:21:18 +0530

    Delete fruits directory

--- fruits/index.js
+++
@@ -1,8 +0,0 @@
-const apple = require("./apple");   //requre all data
-const banana = require("./banana");
-const mango = require("./mango");
-const orange = require("./orange");
-
-let fruits = {apple, orange, mango, banana}  // combind the bunch of data
-
-module.exports = fruits;  // export Data
