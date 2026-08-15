8fe21a6c2e185a1406944b8bb50462125370f9e4
Amit Gupta <amitgupta4263@gmail.com>
2026-08-16 00:10:22 +0530

    modifie:

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const PORT = 5000;
@@ -5,6 +7,10 @@ const PORT = 5000;
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected!'))
+  .catch((err) => console.log('MongoDB connection error:', err));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
